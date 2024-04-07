56bc056c8628cf3f079d4254637f00a178ddcc40
dancolm95 <dancolm95@gmail.com>
2024-04-06 20:41:15 -0500

    fixed file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const FILE_PATH = PROCESS.ARGV[2];
-const STRING_TO_WRITE = PROCESS.ARGV[3];
 
-if (!FILE_PATH || !STRING_TO_WRITE) {
-    console.log("Error: devuelve una ruta válida");
-    process.exit(1);
-}
-fs.writeFile(FILE_PATH, STRING_TO_WRITE, "utf-8", (err) => {
+const DATATOWRITE = PROCESS.ARGV[3];
+
+fs.writeFile(FILE_PATH, DATATOWRITE, 'utf-8', (err) => {
     if (err) {
-        console.log(err);
+        console.error('Error writing file:', err);
+        return;
     }
 });
