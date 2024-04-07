1ec329baec74e6b294e31eb1e94687679d827cf1
dancolm95 <dancolm95@gmail.com>
2024-04-06 20:25:52 -0500

    fixed file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
-   return;
+    console.log(err);
+  } else {
+    process.stdout.write(data);
   }
-    console.log(data);
 });
