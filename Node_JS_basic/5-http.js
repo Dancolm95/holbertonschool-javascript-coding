9393aa9305b8f2da3b427b731c28550e2a74c0f6
dancolm95 <dancolm95@gmail.com>
2024-05-07 19:38:01 -0500

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,34 +1,57 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
+const fs = require('fs');
 const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  const { url } = req;
-
-  if (url === '/') {
-    res.write('Hello Holberton School!');
-  } else if (url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    if (process.argv.length > 2 && fs.existsSync(process.argv[2])) {
+      countStudents(process.argv[2])
+        .then((data) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          const students = data;
+          const out = [];
+          const fields = {}; // All fields availables
+          students.forEach((student) => {
+            fields[student[student.length - 1]] = (fields[student[student.length - 1]] || 0) + 1;
+          });
+          // out.push('This is the list of our students');
+          out.push('This is the list of our students');
+          out.push(`Number of students: ${students.length}`);
+          for (const field in fields) {
+            if (field) {
+              // Filter just the firts names of the students
+              const result = students
+                .filter((std) => std[std.length - 1] === field)
+                .map((el) => el[0]);
+              out.push(`Number of students in ${field}: ${result.length}. List: ${result.join(', ')}`);
+            }
+          }
+          res.end(`${out.join('\n')}`);
+        }).catch((error) => {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end(`Internal Server Error: ${error.message}`);
+        });
+    } else {
+      res.end('This is the list of our students\nCannot load the database');
     }
+  } else {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('This endpoint does not exists');
   }
-  res.statusCode = 404;
-  res.end();
 });
 
-app.listen(port, hostname, () => { });
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
